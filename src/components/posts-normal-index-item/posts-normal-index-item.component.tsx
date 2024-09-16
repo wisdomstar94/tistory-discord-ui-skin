a21eb2a1dbd4a1088e6f7f82fc07c0256d356013
wisdomstar94 <wisdomstar94@gmail.com>
2024-09-16 22:46:53 +0900

    PostsNormalIndexItem 작업 1차 완료.

--- src/components/posts-normal-index-item/posts-normal-index-item.component.tsx
+++ src/components/posts-normal-index-item/posts-normal-index-item.component.tsx
@@ -1,5 +1,6 @@
 import { cn } from "@/utils/cn";
 import { Script } from "../script/script.component";
+import { MessageCircle } from "lucide-react";
 
 export function PostsNormalIndexItem() {
   return (
@@ -50,9 +51,25 @@ export function PostsNormalIndexItem() {
               </div>
             </div>
             <div data-title="article-extra-info" className={cn("w-full flex gap-2 relative items-start text-sm")}>
-              <ul className="w-full flex flex-wrap gap-2 relative">
-                <li className={cn("inline-flex gap-2 relative text-light-color-3/50", "text-xs lg:text-sm")}>
-                  <div>{">"}</div>
+              <ul className={cn("w-full flex flex-wrap relative", "flex-col lg:flex-row", "gap-1 lg:gap-2")}>
+                <li className={cn("inline-flex gap-1 relative text-light-color-3/50", "items-start lg:items-center", "text-xs lg:text-sm")}>
+                  <MessageCircle className="w-[16px] h-[16px] text-light-color-3/80" />
+                  <s_rp_count>
+                    <span className="text-light-color-3/80">[##_article_rep_rp_cnt_##]</span>
+                  </s_rp_count>
+                </li>
+                <li
+                  className={cn(
+                    "gap-2 relative text-light-color-3/50",
+                    "items-start lg:items-center",
+                    "text-xs lg:text-sm",
+                    "hidden lg:inline-flex"
+                  )}
+                >
+                  <div className="w-0.5 h-0.5 rounded-3xl bg-light-color-3/20" />
+                </li>
+                <li className={cn("inline-flex gap-2 relative text-light-color-3/50", "items-start lg:items-center", "text-xs lg:text-sm")}>
+                  {/* <div>{">"}</div> */}
                   <div className={cn("inline-flex relative", "flex-col lg:flex-row", "gap-0 lg:gap-2")}>
                     <span>
                       {"[##_article_rep_date_year_##]-[##_article_rep_date_month_##]-[##_article_rep_date_day_##]"}{" "}
@@ -75,7 +92,15 @@ export function PostsNormalIndexItem() {
               </ul>
             </div>
           </div>
-          <div className="w-[80px] aspect-square block flex-shrink-0 relative bg-light-color-1 rounded-xl overflow-hidden"></div>
+          <s_article_rep_thumbnail>
+            <div className="w-[80px] aspect-square block flex-shrink-0 relative bg-light-color-1 rounded-xl overflow-hidden">
+              <img
+                className="w-full h-full object-cover object-center"
+                alt="포스트글 썸네일 이미지"
+                src="[##_article_rep_thumbnail_url_##]"
+              />
+            </div>
+          </s_article_rep_thumbnail>
         </a>
       </li>
     </>
